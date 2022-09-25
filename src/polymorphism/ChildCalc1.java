79c846feebc34c38f36d718af87d9c24d4b095ef
Bhranti02 <bhranti_patel@yahoo.co.uk>
2022-09-25 20:30:02 +0100

    Revised Homework polymorphism concepts overriding

--- src/polymorphism/ChildCalc1.java
+++ src/polymorphism/ChildCalc1.java
@@ -2,14 +2,15 @@ package polymorphism;
 
 public class ChildCalc1 extends ParentCalc1{
 
-    public void myName(String a, String b){
+    public void myName(int a, int b){
+
         System.out.println(a + b);
     }
 
     public static void main(String[] args) {
 
         ChildCalc1 obj1 = new ChildCalc1();
-        obj1.myName("Pratik", "kumar"); // overriding parent class myName method,& execute child class myName method
+        obj1.myName(10,5); // overriding parent class myName method,& execute child class myName method
 
      }
 
--- src/polymorphism/ParentCalc1.java
+++ src/polymorphism/ParentCalc1.java
@@ -2,15 +2,10 @@ package polymorphism;
 
 public class ParentCalc1 {
 
-    public void myName(String a, String b){
-        System.out.println(a + "  " + b);
+    public void myName(int a, int b){
+        System.out.println(a - b);
     }
 
-    public static void main(String[] args) {
-        ParentCalc1 obj1 = new ParentCalc1();
 
-        obj1.myName("Bhranti", "Patel");
-
-    }
 
 }
